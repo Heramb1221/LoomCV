cb476d3a77bb800a453d75fd3d916bd5e28619f3
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-08-16 01:46:14 +0530

    Setup complete

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from '@clerk/nextjs';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Loomcv",
+    absolute: "LoomCV",
+  },
+  description:
+    "LoomCV is the easiest way to create a professional resume that will help you land your dream job.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
+    
+  );
+}
