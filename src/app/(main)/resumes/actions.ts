cdfcb15ac1f2bcf11465fffca4590ddb52991f00
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-08-20 18:18:01 +0530

    Premium Button

--- src/app/(main)/resumes/actions.ts
+++ src/app/(main)/resumes/actions.ts
@@ -1,37 +1,37 @@
-"use server"
+"use server";
 
 import prisma from "@/lib/prisma";
-import { auth } from "@clerk/nextjs/server"
+import { auth } from "@clerk/nextjs/server";
 import { del } from "@vercel/blob";
 import { revalidatePath } from "next/cache";
 
 export async function deleteResume(id: string) {
-    const {userId} = await auth();
+  const { userId } = await auth();
 
-    if(!userId) {
-        throw new Error("User not authenticated");
-    }
+  if (!userId) {
+    throw new Error("User not authenticated");
+  }
 
-    const resume = await prisma.resume.findUnique({
-        where: {
-            id,
-            userId
-        }
-    })
+  const resume = await prisma.resume.findUnique({
+    where: {
+      id,
+      userId,
+    },
+  });
 
-    if(!resume) {
-        throw new Error("Resume not found.")
-    }
+  if (!resume) {
+    throw new Error("Resume not found");
+  }
 
-    if(resume.photoUrl) {
-        await del(resume.photoUrl)
-    }
+  if (resume.photoUrl) {
+    await del(resume.photoUrl);
+  }
 
-    await prisma.resume.delete({
-        where: {
-            id
-        }
-    })
+  await prisma.resume.delete({
+    where: {
+      id,
+    },
+  });
 
-    revalidatePath("/resumes");
+  revalidatePath("/resumes");
 }
