ae8809416d66259a3dbfc03d5eb094f2871cdebd
Heramb Chaudhari <169079872+Heramb1221@users.noreply.github.com>
2025-10-01 14:42:28 +0530

    google gemini name changes openai.ts

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,5 +1,5 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 
-export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+export const genAI = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY! });
 
-export const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+export const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
